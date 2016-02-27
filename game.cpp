cffa1ba126e59e00e0452b21c5f515f63a7732df
Shchurov Alexey <alexiskhb@gmail.com>
2016-02-27 11:38:01 +1000

    added entities

--- game.cpp
+++ game.cpp
@@ -16,9 +16,8 @@
 #define CMD_DOWN   's'
 #define CMD_LEFT   'a'
 #define CMD_RIGHT  'd'
-#define CMD_ATTACK 'r'
-#define CMD_FBALL  'f'
-#define CMD_RANDOM 'r'
+#define CMD_ATTACK 'f'
+#define CMD_MAGIC  'r'
 
 // first knight's turn, 
 // then zombies and dragon can attack him.
@@ -30,6 +29,14 @@ using namespace std;
 bool game_over = false;
 
 
+class Map {
+public:
+	void display() {
+	}
+	Object map[22][22];
+};
+
+
 class Object {
 public:
 	Object() {
@@ -39,8 +46,10 @@ public:
 	}
 
 	virtual char symbol() {
-		return EMPTY;
+		return SYM_EMPTY;
 	}
+
+	virtual bool is_permeable();
 
 protected:
 	int x = -1;
@@ -50,12 +59,65 @@ protected:
 };
 
 
+class Wall : Object {
+public:
+	bool is_permeable() {
+		return false;
+	}
+
+};
+
+
+class Flame : Object {
+public:
+	bool is_permeable(){
+		return true;
+	}
+};
+
+
+class Swamp : Object {
+public:
+	char symbol() {
+		return SYM_SWAMP;
+	}
+
+	bool is_permeable() {
+		return true;
+	}
+
+};
+
+// magic can heal
+class Magic : Object {
+public:
+	Magic() {
+		lifetime = 5;
+	}
+
+	Magic(int x_coord, int y_coord) : Object(x_coord, y_coord) {
+		lifetime = 5;
+	}
+
+	char symbol() {
+		return SYM_MAGIC;
+	}
+
+	bool is_permeable() {
+		return true;
+	}
+
+};
+
 class Character : Object {
 public:
-	Character() : Object() {
+	Character() {
 	}
 
 	Character(int x_coord, int y_coord) : Object(x_coord, y_coord) {
+	}
+
+	Character(int x_coord, int y_coord, int hp, int dmg) : Object(x_coord, y_coord), health(hp), damage(dmg) {
 	}
 
 	virtual void move();
@@ -63,6 +125,11 @@ public:
 	virtual char attack();
 	// returns True if Character died
 	virtual bool suffer(int dmg);
+
+	bool is_permeable() {
+		return false;
+	}
+
 protected:
 	int health = 1;
 	int damage = 0;
@@ -70,18 +137,22 @@ protected:
 
 
 class Knight : Character {
+public:
 	char symbol() {
-		return KNIGHT;
+		return SYM_KNIGHT;
 	}
 
-private:
+	void move() {
+		char action;
+		cin >> action;
+	}
 
 };
 
 
 class Princess : Character {
 	char symbol() {
-		return PRINCESS;
+		return SYM_PRINCESS;
 	}
 
 };
@@ -94,35 +165,44 @@ class Monster : Character {
 
 class Dragon : Monster {
 	char symbol() {
-		return DRAGON;
+		return SYM_DRAGON;
 	}
-
 };
 
 
 class Zombie : Monster {
 	char symbol() {
-		return ZOMBIE;
+		return SYM_ZOMBIE;
 	}
 };
 
 
-struct Map {
-	void display() {
+struct {
+	vector<Character> characters;
+	vector<Object>    objects;
+
+	bool is_over() {
+		return false;
+	}
+
+	void draw_objects() {
 
 	}
-	Object map[22][22];
 
-};
+	void draw_characters() {
+
+	}
+
+	void next_turn() {
+
+	}
+
+} Game;
 
 
 int main(int argc, char** argv) {
-	char turn;
- 	while (!game_over) {
- 		cin >> turn;
- 		if (turn == QUIT) {
- 			break;
- 		}
+ 	while (!Game.is_over()) {
+ 		Game.next_turn();
  	}
 }
 

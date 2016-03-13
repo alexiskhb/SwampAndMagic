1229ab834df874bb859f3941f5e813526e1604ea
Shchurov Alexey <alexiskhb@gmail.com>
2016-03-13 18:19:59 +1000

    added BANode in BilateralArray

--- bilateral_array.h
+++ bilateral_array.h
@@ -2,47 +2,82 @@
 
 #include <vector>
 #include <cmath>
+#include <iterator>
+#include <algorithm>
+#include <limits>
 
-template <typename Type>
+template <class Type> class BilateralArray;
+template <class Type> class BAIterator;
+template <class Type> class BANode;
+
+
+template <class Type> 
+class BANode {
+public:
+	BANode() : value(Type()), index(0), is_used(false) {
+	}
+
+	BANode(const Type& elem, const int aindex) : value(elem), index(aindex), is_used(true) {
+	}
+
+	~BANode() {
+	}
+
+	Type value;
+	int  index;
+	bool is_used;
+};
+
+
+
+
+
+template <class Type>
 class BilateralArray {
 public:
 	BilateralArray() {
-
+		negative.push_back(BANode<Type>(Type(), 0));
 	}
 
-	void insert(const int index, Type element) {
+	std::vector<BANode<Type>>& semiaxis(const int index) {
+		return index >= 0 ? positive : negative;
+	}
+
+	void update_minmax(const int index) {
+		min_index = std::min(min_index, index);
+		max_index = std::max(max_index, index);
+	}
+
+	void insert(const int index, Type value) {
 		expand_for(index);
-		std::vector<Type>& semiaxis      = index >= 0 ? positive : negative;
-		std::vector<Type>& semiaxis_used = index >= 0 ? pos_used : neg_used;
-		semiaxis.at(abs(index)) = element;
-		semiaxis_used.at(abs(index)) = true;
+		semiaxis(index).at(abs(index)) = BANode<Type>(value, index);
 	}
 
-	unsigned int size() {
+	unsigned int size() const {
 		return positive.size() + (negative.size() > 1 ? negative.size() - 1 : 0);
 	}
 
-	bool includes(const int index) {
-		std::vector<Type>& semiaxis = index >= 0 ? positive : negative;
-		return semiaxis.size() > abs(index);
+	bool includes(const int index) const {
+		return semiaxis(index).size() > abs(index);
+	}
+
+	Type& at(const int index) {
+		expand_for(index);
+		return semiaxis(index).at(abs(index)).value;
 	}
 
 	Type& operator[](const int index) {
-		expand_for(index);
-		std::vector<Type>& semiaxis = index >= 0 ? positive : negative;
-		return semiaxis.at(abs(index));
+		return at(index);
 	}
 
-	// Does nothing if element at index already available.
-	// Returns reference to element at index, NOT this!
+	// Does nothing if value at index already available.
+	// Returns reference to value at index, NOT this!
 	Type& expand_for(const int index) {
-		std::vector<Type>& semiaxis      = index >= 0 ? positive : negative;
-		std::vector<bool>& semiaxis_used = index >= 0 ? pos_used : neg_used;
-		if (abs(index) >= semiaxis.size()) {
-			semiaxis.resize(abs(index) + 1);
-			semiaxis_used.resize(abs(index) + 1);
+		if (abs(index) >= semiaxis(index).size()) {
+			semiaxis(index).resize(abs(index) + 1);
 		}
-		return semiaxis.at(abs(index));
+		update_minmax(index);
+		return semiaxis(index).at(abs(index)).value;
 	}
 
 	// Does the same as expand_for(), but returns *this.
@@ -52,19 +87,35 @@ public:
 	}
 
 	bool is_used(const int index) {
-		expand_for(index);
-		std::vector<bool>& semiaxis_used = index >= 0 ? pos_used : neg_used;
-		return semiaxis_used.at(abs(index));
+		return includes(index) && semiaxis(index).at(abs(index)).is_used;
 	}
+
+	void push_back(const Type&& value) {
+		update_minmax(positive.size() - 1);
+		positive.push_back(BANode<Type>(value, positive.size() - 1));
+	}
+
+	void push_front(const Type&& value) {
+		update_minmax(negative.size() - 1);
+		negative.push_back(BANode<Type>(value, negative.size() - 1));
+	}
+
+	int biggest_index() {
+		return max_index;
+	}
+
+	
 private:
 	// Zero is in positive 
-	std::vector<Type> negative, positive;
-	std::vector<bool> neg_used, pos_used;
+	std::vector<BANode<Type>> negative, positive;
+	int min_index = std::numeric_limits<int>::max();
+	int max_index = std::numeric_limits<int>::min();
 };
 
 
 
-template <typename Type>
+
+template <class Type>
 class BilateralArray2D {
 public:
 	BilateralArray2D() {
